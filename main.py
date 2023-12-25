e2836913f628d95e72504547575cb5c555a4eb88
Maga <maga@LAPTOP-1I6JJP9Q>
2023-12-26 01:13:11 +0300

    repair some main

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+
 import pandas as pd
 import streamlit as st
+import streamlit.components.v1 as components
+import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
-import seaborn as sns
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
 def some_predicts(data):
+        # Удалите целевую переменную (target)
     # Удалите целевую переменную (target)
     X = data.drop('TARGET', axis=1)
     y = data['TARGET']
@@ -32,7 +35,7 @@ def some_predicts(data):
         classifier.fit(X_train, y_train)
         y_pred = classifier.predict(X_test)
         accuracy = accuracy_score(y_test, y_pred)
-        classification_rep = classification_report(y_test, y_pred, zero_division=1)
+        classification_rep = classification_report(y_test, y_pred)
         results[name] = {'accuracy': accuracy, 'classification_report': classification_rep}
 
     # Выведите результаты
@@ -40,6 +43,9 @@ def some_predicts(data):
         st.subheader(name)
         st.write(f"Accuracy: {result['accuracy']:.2f}")
         st.text(f"Classification Report\nchr {result['classification_report']}")
+    
+    
+
 
 def custom_eda(data):
     # Реализуйте здесь ваш кастомный EDA
@@ -50,13 +56,21 @@ def custom_eda(data):
     st.write(data.describe())
 
     # Выведите распределения всех признаков в виде каскада графиков
-    for column in data.columns:
-        st.subheader(f'Распределение {column}')
-        st.pyplot(sns.histplot(data[column], kde=True, color='skyblue').figure)
+    st.set_option('deprecation.showPyplotGlobalUse', False)  # Для предотвращения предупреждения о deprecation
+    fig, axes = plt.subplots(nrows=len(data.columns), ncols=1, figsize=(8, 2 * len(data.columns)))
+
+    for i, column in enumerate(data.columns):
+        sns.histplot(data[column], kde=True, ax=axes[i], color='skyblue')
+        axes[i].set_title(f'Распределение {column}')
+
+    plt.tight_layout()
+    st.pyplot(fig)
 
     corr = data.corr()
-    st.subheader('Матрица корреляции')
-    st.pyplot(sns.heatmap(corr, annot=True, cmap='coolwarm', fmt=".2f", linewidths=.5).figure)
+    fig = plt.figure(figsize=(10, 8))
+    sns.heatmap(corr, annot=True, cmap='coolwarm', fmt=".2f", linewidths=.5)
+    plt.title('Матрица корреляции')
+    st.pyplot(fig)
 
     # Выведите виджеты для выбора признаков
     selected_feature = st.selectbox("Выберите признак, чтобы построить график распределения:", data.columns)
@@ -65,18 +79,27 @@ def custom_eda(data):
     # Определите количество уникальных значений в выбранном признаке
     unique_values_count = data[selected_feature].nunique()
 
-    # Выберите тип графика в зависимости от количества уникальных значений
+# Выберите тип графика в зависимости от количества уникальных значений
     if unique_values_count <= 10:
         # Гистограмма для малого количества уникальных значений
-        st.subheader(f'Распределение {selected_feature} по классам {target_variable}')
-        st.pyplot(sns.countplot(x=selected_feature, hue=target_variable, data=data, palette='pastel', dodge=True).figure)
+        fig = plt.figure(figsize=(10, 6))
+        sns.countplot(x=selected_feature, hue=target_variable, data=data, palette='pastel', dodge=True)
+        plt.title(f'Распределение {selected_feature} по классам {target_variable}')
+        plt.xlabel(selected_feature)
+        plt.ylabel('Частота')
+        plt.legend(title=target_variable)
+        st.pyplot(fig)
     else:
         # Обычный график для большого количества уникальных значений
-        st.subheader(f'Boxplot {selected_feature} по классам {target_variable}')
-        st.pyplot(sns.boxplot(x=target_variable, y=selected_feature, data=data, hue=target_variable, palette='pastel', legend=False).figure)
+        fig = plt.figure(figsize=(12, 6))
+        sns.boxplot(x=target_variable, y=selected_feature, data=data, palette='pastel')
+        plt.title(f'Boxplot {selected_feature} по классам {target_variable}')
+        plt.xlabel(target_variable)
+        plt.ylabel(selected_feature)
+        st.pyplot(fig)
 
     some_predicts(data)
-
+    
 def main():
     st.title("Кастомный EDA с Streamlit")
 
@@ -86,14 +109,14 @@ def main():
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
     else:
-        # В противном случае используйте свой датасет (замените "ваш_файл.csv" на фактический путь)
+    # В противном случае используйте свой датасет (замените "ваш_файл.csv" на фактический путь)
         df = pd.read_csv('data.csv')
 
-    # Отображение данных
+        # Отображение данных
     st.write("Первые 5 строк датафрейма:")
     st.write(df.head())
 
-    # Выполнение кастомного EDA
+        # Выполнение кастомного EDA
     custom_eda(df)
 
 if __name__ == "__main__":
